1528a3e83859dc8602536759066bcb5275076f86
Richard <48015496+arthuRHD@users.noreply.github.com>
2020-05-03 13:48:10 +0200

    Update production.py

--- core/production.py
+++ core/production.py
@@ -8,6 +8,7 @@ MYSQL_PORT = '3306'
 ELASTICSEARCH_HOST = 'localhost:9200'
 
 ALLOWED_HOSTS = ['portfolio.richardinfo.fr', '192.168.1.61']
+SECRET_KEY = 'yb&)xnt!nb)@66-t)_bxmq8rk=l+2un)$c#s9)=jgrcs)mwq_-'
 
 STATIC_URL = "/static/"
 
